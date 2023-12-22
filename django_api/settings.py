a32f78ba1895aa7438238d0a9b2d68df5f6f264a
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-12-22 12:13:50 +0530

    update Project

--- django_api/settings.py
+++ django_api/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'django_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mytestdb_django',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
